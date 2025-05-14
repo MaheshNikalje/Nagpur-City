1c05144f246426b1f1ba783f3c8ee1e654bab6f1
Vrutantr17 <bittu.rangari1701@gmail.com>
2025-05-14 16:24:13 +0530

    added tailwind and react icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { FaCity } from 'react-icons/fa'
+import { PiAirplane } from 'react-icons/pi'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <FaCity />
+    <PiAirplane />
+    </>
   )
 }
 
