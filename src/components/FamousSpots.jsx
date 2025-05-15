944c240691e7e67e801e8494930d82e3e0e330ca
saqlein sheikh <suqlein@gmail.com>
2025-05-15 14:00:57 +0530

    Your commit message here

--- src/components/FamousSpots.jsx
+++ src/components/FamousSpots.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const FamousSpots = () => {
   return (
-    <div>FamousSpots My spots</div>
+    <>
+    <div className='ml-5'>
+      <img src="/deekshabhoomi.png" alt="" className='w-150 ml- rounded-2xl'  />
+      <h1 className='ml- font-bold'>Deekshabhoomi</h1>
+      <h1 className='ml- font-bold'> What is Deekshabhoomi?</h1>
+      <p>Deekshabhoomi is the sacred monument where Dr. B.R. Ambedkar converted to Buddhism along with over 600,000 followers on 14th October 1956. It marked a major social and spiritual movement in India known as the Dalit Buddhist Movement.</p>
+    </div>
+    </>
   )
 }
 
